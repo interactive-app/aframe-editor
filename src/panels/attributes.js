cb0399bf5acab376828882acc10af00a3f4e8ff6
Fernando Serrano <fernandojsg@gmail.com>
2015-12-16 01:07:41 +0100

    Refactoring and comments added

--- src/panels/attributes.js
+++ src/panels/attributes.js
@@ -14,13 +14,21 @@ function handleEntityChange (entity, name, property, value) {
 }
 
 function Attributes (editor) {
-  var ignoreComponentsChange = false;
-  var container = new UI.Panel();
-  container.setBorderTop('0');
-  container.setPaddingTop('20px');
-  container.setDisplay('none');
-
+  var objectId, objectType, objectCustomRow;
+  var componentsList;
   var widgets = {};
+  var ignoreComponentsChange = false;
+
+  /**
+   * Creates and returns a widget based on the type of the attribute
+   * If a schema is provided it's used to set min/max values or populate the combobox values.
+   * @param {string} componentName   Name of the component that has this attribute (e.g: 'geometry')
+   * @param {string} attributeName   Attribute name in the component (e.g: 'primitive')
+   * @param {string} property        Property name in case of multivalues attributes (e.g: 'x')
+   * @param {string} type            Type of the widget to generate (e.g: 'checkbox')
+   * @param {JSON} parameterSchema [Optional] JSON with the schema definition of the attribute.
+   * @return {UI.Widget} Returns an UI.js widget based on the type and schema of the attribute.
+   */
   function addAttribute (componentName, attributeName, property, type, parameterSchema) {
     var widget = null;
     if (typeof parameterSchema === 'undefined') {
@@ -60,17 +68,28 @@ function Attributes (editor) {
     if (parameterSchema.hasOwnProperty('max')) {
       widget.max = parameterSchema.max;
     }
-    widget.schema = parameterSchema; // Hack
+    widget.schema = parameterSchema;
     widget.onChange(function (event) {
       update(event, componentName, attributeName, property);
     });
 
+    // Generate an unique ID for this attribute (e.g: geometry.primitive)
+    // and save it on the widgets variable so we could easily access to it in the following functions
     var id = attributeName ? componentName + '.' + attributeName + '.' + property : property ? (componentName + '.' + property) : componentName;
     widgets[id] = widget;
     return widget;
   }
 
-  var objectId, objectType;
+  /**
+   * Generates a container with the common attributes and components for each entity:
+   *   - type
+   *   - ID
+   *   - position
+   *   - rotation
+   *   - scale
+   *   - visible
+   * @return {UI.CollapsiblePanel} Panel containing all the widgets
+   */
   function generateCommonAttributes () {
     var container = new UI.CollapsiblePanel();
 
@@ -142,8 +161,22 @@ function Attributes (editor) {
     return container;
   }
 
-  var componentsList;
-  function generateNewComponentsRow () {
+  /**
+   * Add component to the entity
+   * @param {a-entity} entity        Entity
+   * @param {string} componentName Component name
+   */
+  function addComponent (entity, componentName) {
+    entity.setAttribute(componentName, '');
+    generateComponentsPanels(entity);
+    updateUI(entity);
+  }
+
+  /**
+   * Generate a row including a combobox with the available components to add to
+   * the current entity
+   */
+  function generateAddComponentRow () {
     var componentsRow = new UI.Row();
     var componentsOptions = {};
     var ignoredComponents = ['position', 'rotation', 'scale', 'visible'];
@@ -153,46 +186,21 @@ function Attributes (editor) {
       }
     }
 
-    function addComponent (componentName) {
-      var entity = editor.selected.el;
-      entity.setAttribute(componentName, '');
-      updateRows(entity);
-      updateUI(entity);
-    }
-
     componentsList = new UI.Select().setId('componentlist').setOptions(componentsOptions).setWidth('150px');
     componentsRow.add(new UI.Text('Add').setWidth('90px'));
     componentsRow.add(componentsList);
     var button = new UI.Button('+').onClick(function () {
-      addComponent(componentsList.getValue());
+      // Add the selected component from the combobox to the current active entity
+      addComponent(editor.selected.el, componentsList.getValue());
     });
     componentsRow.add(button.setWidth('20px'));
     return componentsRow;
   }
 
-  container.add(generateCommonAttributes());
-  container.add(generateNewComponentsRow());
-
-  // --- CUSTOM
-  var objectCustomRow = new UI.Row();
-  container.add(objectCustomRow);
-
-  editor.signals.entitySelected.add(function (entity) {
-    if (entity) {
-      container.show();
-      updateRows(entity);
-      updateUI(entity);
-    } else {
-      container.hide();
-    }
-  });
-
-  editor.signals.componentChanged.add(function (evt) {
-    var entity = evt.detail.target;
-    updateUI(entity);
-    editor.signals.objectChanged.dispatch(entity.object3D);
-  });
-
+  /**
+   * Update the UI widgets based on the current entity & components values
+   * @param  {a-entity} entity Entity currently selected
+   */
   function updateUI (entity) {
     if (ignoreComponentsChange) {
       return;
@@ -201,19 +209,22 @@ function Attributes (editor) {
     objectType.setValue(entity.tagName);
     objectId.setValue(entity.id);
 
+    // Disable the components already used form the list of available
+    // components to add to this entity
     var availableComponents = componentsList.dom.querySelectorAll('option');
     for (var i = 0; i < availableComponents.length; i++) {
       availableComponents[i].disabled = entity.getAttribute(availableComponents[i].value);
     }
 
-    var attributes = Array.prototype.slice.call(entity.attributes);
-    attributes.forEach(function (attribute) {
-      var properties = entity.getAttribute(attribute.name);
-      for (var property in properties) {
-        var id = attribute.name + '.' + property;
+    // Update the value of the widgets based on the entity's components's attributes
+    var components = Array.prototype.slice.call(entity.attributes);
+    components.forEach(function (component) {
+      var attributes = entity.getAttribute(component.name);
+      for (var attribute in attributes) {
+        var id = component.name + '.' + attribute;
         var widget = widgets[id];
         if (widget) {
-          widget.setValue(properties[property]);
+          widget.setValue(attributes[attribute]);
         }
       }
     });
@@ -221,8 +232,11 @@ function Attributes (editor) {
     updateWidgetVisibility(entity);
   }
 
+  /**
+   * Update the widgets visibility based on the 'if' attribute from theirs attribute' schema
+   * @param  {a-entity} entity Entity currently selected
+   */
   function updateWidgetVisibility (entity) {
-    // Apply visibility
     for (var componentName in entity.components) {
       var properties = aframeCore.components[componentName].schema;
       for (var property in properties) {
@@ -248,14 +262,67 @@ function Attributes (editor) {
     }
   }
 
+  /**
+   * Reset to default (clear) one entity's component
+   * @param {a-entity} entity        Entity
+   * @param {string} componentName Component name to clear
+   */
   function setEmptyComponent (entity, componentName) {
     entity.setAttribute(componentName, '');
-    updateRows(entity);
+    generateComponentsPanels(entity);
     updateUI(entity);
     editor.signals.objectChanged.dispatch(entity.object3D);
   }
 
-  function updateRows (entity) {
+  /**
+   * Generates a row containing the parameter label and its widget
+   * @param {string} componentName   Component name
+   * @param {string} component   Component element
+   * @param {string} parameterName   Parameter name
+   * @param {object} parameterSchema Parameter schema
+   */
+  function addParameterRow (componentName, component, parameterName, parameterSchema) {
+    var newParamRow = new UI.Row();
+    newParamRow.add(new UI.Text(parameterName).setWidth('120px'));
+
+    var defaultValue = parameterSchema.default;
+    var type = null;
+    if (parameterSchema.oneOf) {
+      type = 'select';
+    } else {
+      switch (typeof defaultValue) {
+        case 'boolean':
+          type = 'checkbox';
+          break;
+        case 'number':
+          type = 'number';
+          break;
+        case 'object':
+          type = 'vector3';
+          break;
+        case 'string':
+          if (defaultValue.indexOf('#') === -1) {
+            type = 'input';
+          } else {
+            type = 'color';
+          }
+          break;
+        default:
+          console.warn(parameterName, component.defaults[parameterName], typeof component.defaults[parameterName]);
+      }
+    }
+    var newWidget = addAttribute(componentName, null, parameterName, type, parameterSchema);
+    newWidget.setValue(defaultValue);
+    newWidget.parameterRow = newParamRow;
+    newParamRow.add(newWidget);
+    return newParamRow;
+  }
+
+  /**
+   * Generate an UI.CollapsiblePanel for each entity's component
+   * @param  {a-entity} entity Current selected entity
+   */
+  function generateComponentsPanels (entity) {
     var componentsToIgnore = [
       'position', 'rotation', 'scale', 'visible'
     ];
@@ -263,103 +330,69 @@ function Attributes (editor) {
     objectCustomRow.clear();
 
     for (var componentName in entity.components) {
+      // Ignore the components that we've already included on the common attributes panel
       if (componentsToIgnore.indexOf(componentName) !== -1) {
         continue;
       }
 
       var component = entity.components[componentName];
-      var componentSchema = aframeCore.components[componentName].schema;
 
-      var objectActions = new UI.Select().setId(componentName).setPosition('absolute').setRight('8px').setFontSize('11px');
-      objectActions.setOptions({
-        'Actions': 'Actions',
-        'Delete': 'Delete',
-        'Clear': 'Clear'
-        // 'Reset': 'Reset to initial'
-      });
+      // Add a context menu to delete or reset the component
+      var objectActions = new UI.Select()
+        .setId(componentName)
+        .setPosition('absolute')
+        .setRight('8px')
+        .setFontSize('11px')
+        .setOptions({
+          'Actions': 'Actions',
+          'Delete': 'Delete',
+          'Clear': 'Clear'
+        })
+        .onClick(function (event) {
+          event.stopPropagation(); // Avoid panel collapsing
+        })
+        .onChange(function (event, component) {
+          var action = this.getValue();
+          switch (action) {
+            case 'Delete':
+              entity.removeAttribute(this.getId());
+              break;
 
-      objectActions.onClick(function (event) {
-        event.stopPropagation(); // Avoid panel collapsing
-      });
-      objectActions.onChange(function (event, component) {
-        var action = this.getValue();
-        switch (action) {
-          case 'Delete':
-            entity.removeAttribute(this.getId());
-            break;
+            case 'Clear':
+              setEmptyComponent(entity, this.getId());
+              break;
 
-          case 'Clear':
-            setEmptyComponent(entity, this.getId());
-            break;
+            default:
+              return;
+          }
+          this.setValue('Actions');
+          generateComponentsPanels(entity);
+          updateUI(entity);
+          editor.signals.objectChanged.dispatch(entity.object3D);
+        });
 
-          default:
-            return;
-        }
-        this.setValue('Actions');
-        updateRows(entity);
-        updateUI(entity);
-        editor.signals.objectChanged.dispatch(entity.object3D);
-      });
+      // Collapsible panel with component name as title
       var container = new UI.CollapsiblePanel();
       container.addStatic(new UI.Text(componentName).setTextTransform('uppercase'), objectActions);
       container.add(new UI.Break());
 
-      function addParameterRow (parameterName, parameterSchema) {
-        var newParamRow = new UI.Row();
-        newParamRow.add(new UI.Text(parameterName).setWidth('120px'));
-
-        var defaultValue = parameterSchema.default;
-        var type = null;
-        if (parameterSchema.oneOf) {
-          type = 'select';
-        } else {
-          switch (typeof defaultValue) {
-            case 'boolean':
-              type = 'checkbox';
-              break;
-            case 'number':
-              type = 'number';
-              break;
-            case 'object':
-              type = 'vector3';
-              break;
-            case 'string':
-              if (defaultValue.indexOf('#') === -1) {
-                type = 'input';
-              } else {
-                type = 'color';
-              }
-              break;
-            default:
-              console.warn(parameterName, component.defaults[parameterName], typeof component.defaults[parameterName]);
-          }
-        }
-        var newWidget = addAttribute(componentName, null, parameterName, type, parameterSchema);
-        newWidget.setValue(defaultValue);
-        newWidget.parameterRow = newParamRow;
-        newParamRow.add(newWidget);
-        return newParamRow;
-      }
-      for (var parameterName in componentSchema) {
-        container.add(addParameterRow(parameterName, componentSchema[parameterName]));
+      // Add a widget's row for each parameter on the component
+      for (var parameterName in component.schema) {
+        container.add(addParameterRow(componentName, component, parameterName, component.schema[parameterName]));
       }
 
-      /*
-      console.log(component,componentSchema);
-      if (typeof component.defaults === 'object') {
-        for (var parameterName in component.defaults) {
-          container.add(addParameterRow(parameterName, component.defaults[parameterName]));
-        }
-      } else {
-        // Handle simple type defaults
-        container.add(addParameterRow(null, component.defaults));
-      }
-      */
       container.add(new UI.Break());
       objectCustomRow.add(container);
     }
   }
 
+  /**
+   * Callback when a widget value is updated so we could update the entity attributes
+   * @param  {EventTarget} event         Event generated by the onChange listener
+   * @param  {string} componentName Component name being modified (eg: 'geometry')
+   * @param  {string} attributeName Attribute name being modified (eg: 'primitive')
+   * @param  {string} property      Property name, if any, being modified (eg: 'x')
+   */
   function update (event, componentName, attributeName, property) {
     ignoreComponentsChange = true;
     var entity = editor.selected.el;
@@ -374,6 +407,38 @@ function Attributes (editor) {
     editor.signals.objectChanged.dispatch(entity.object3D);
     ignoreComponentsChange = false;
   }
+
+  // Generate main attributes panel
+  var container = new UI.Panel();
+  container.setBorderTop('0');
+  container.setPaddingTop('20px');
+  container.setDisplay('none');
+
+  // Add common attributes panel (type, id, position, rotation, scale, visible)
+  container.add(generateCommonAttributes());
+
+  // Append the components list that the user can add to the selected entity
+  container.add(generateAddComponentRow());
+
+  // Empty row used to append the panels from each component
+  objectCustomRow = new UI.Row();
+  container.add(objectCustomRow);
+
+  // Signal dispatchers
+  editor.signals.entitySelected.add(function (entity) {
+    if (entity) {
+      container.show();
+      generateComponentsPanels(entity);
+      updateUI(entity);
+    } else {
+      container.hide();
+    }
+  });
+  editor.signals.componentChanged.add(function (evt) {
+    var entity = evt.detail.target;
+    updateUI(entity);
+    editor.signals.objectChanged.dispatch(entity.object3D);
+  });
 
   return container;
 }
