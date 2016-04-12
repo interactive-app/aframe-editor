1485d2fbadd710ea3aa42c53f9ec934dc025634d
Fernando Serrano <fernandojsg@gmail.com>
2016-04-12 14:31:46 +0200

    Added samples

--- src/dialogs/assets.js
+++ src/dialogs/assets.js
@@ -1,4 +1,18 @@
 var UI = require('../../lib/vendor/ui.js'); // @todo will be replaced with the npm package
+var samples = {
+  "textures": [
+    '758px-Canestra_di_frutta_(Caravaggio).jpg',
+    '2294472375_24a3b8ef46_o.jpg',
+    'brick_bump.jpg',
+    'brick_diffuse.jpg',
+    'checkerboard.jpg',
+    'crate.gif',
+    'envmap.png',
+    'grasslight-big.jpg',
+    'sprite0.png',
+    'UV_Grid_Sm.jpg'
+  ]
+};
 
 function AssetsDialog (editor) {
   var container = new UI.Panel();
@@ -21,41 +35,55 @@ function AssetsDialog (editor) {
     select(event.target.textContent);
   }
   var assetsContent = new UI.Panel();
+  var samplesContent = new UI.Panel();
+  var newContent = new UI.Panel();
 
   var tabsContent = new UI.Span().add(assetsContent);
-
   container.add(tabsContent);
+  container.add(samplesContent);
+  container.add(newContent);
+
+  function getImageWidget(texture, mapWidget) {
+    var row = new UI.Row();
+    var img = document.createElement('img');
+    img.src = texture;
+    img.style.width = '100px';
+    img.style.height = '100px';
+    row.dom.appendChild(img);
+
+    var text = document.createTextNode(texture);
+    row.dom.appendChild(text);
+
+    var button = document.createElement('input');
+    button.setAttribute('type', 'button');
+    button.setAttribute('value', 'select');
+    (function (_texture) {
+      button.addEventListener('click', function (event) {
+        mapWidget.setValue('url(' + _texture + ')');
+        if (mapWidget.onChangeCallback) {
+          mapWidget.onChangeCallback();
+        }
+        editor.signals.hideModal.dispatch(assetsContent);
+      });
+    })(texture);
+    row.dom.appendChild(button);
+    return row;
+  }
 
   editor.signals.showAssetsDialog.add(function (mapWidget) {
+    // Assets content
     assetsContent.clear();
     for (var texture in editor.sceneEl.systems.material.textureCache) {
-      var row = new UI.Row();
-      var img = document.createElement('img');
-      img.src = texture;
-
-      // @todo use class
-      img.style.width = '100px';
-      img.style.height = '100px';
-      row.dom.appendChild(img);
-
-      var text = document.createTextNode(texture);
-      row.dom.appendChild(text);
-
-      var button = document.createElement('input');
-      button.setAttribute('type', 'button');
-      button.setAttribute('value', 'select');
-      (function (_texture) {
-        button.addEventListener('click', function (event) {
-          mapWidget.setValue('url(' + _texture + ')');
-          if (mapWidget.onChangeCallback) {
-            mapWidget.onChangeCallback();
-          }
-          editor.signals.hideModal.dispatch(assetsContent);
-        });
-      })(texture);
-      row.dom.appendChild(button);
-
+      var row = getImageWidget(texture, mapWidget);
       assetsContent.add(row);
+    }
+
+    // Assets content
+    samplesContent.clear();
+    for (var i in samples['textures']) {
+      console.log('../assets/textures/'+samples['textures'][i]);
+      var row = getImageWidget('../assets/textures/'+samples['textures'][i], mapWidget);
+      samplesContent.add(row);
     }
 
     // New content
@@ -83,32 +111,19 @@ function AssetsDialog (editor) {
     editor.signals.showModal.dispatch(container);
   });
 
-/*
-  this.sceneGraph = new SceneGraph(editor);
-  this.attributes = new Attributes(editor);
-
-  var scene = new UI.Span().add(
-    this.sceneGraph,
-    this.attributes
-  );
-
-  container.add(scene);
-*/
-
-  var newContent = new UI.Panel();
-
-  container.add(newContent);
-
   function select (section) {
     samplesTab.setClass('');
     assetsTab.setClass('');
+    samplesTab.setClass('');
 
     assetsContent.setDisplay('none');
+    samplesContent.setDisplay('none');
     newContent.setDisplay('none');
 
     switch (section) {
       case 'SAMPLES':
         samplesTab.setClass('selected');
+        samplesContent.setDisplay('');
         break;
       case 'ASSETS':
         assetsTab.setClass('selected');
